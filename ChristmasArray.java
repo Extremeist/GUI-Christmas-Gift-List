e8a59861f9320bb211321ba6ad538826ac681f8b
Lloyd Portes <56543588+Extremeist@users.noreply.github.com>
2020-12-06 22:31:00 +0000

    Add files via upload

---
+++ ChristmasArray.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guichristmaslist;
+/**
+ *
+ * @author porte
+ */
+public class ChristmasArray {
+    public String Url="";
+    public String ChristmasGift="";
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String Url) {
+        this.Url = Url;
+    }
+
+    public String getChristmasGift() {
+        return ChristmasGift;
+    }
+
+    public void setChristmasGift(String ChristmasGift) {
+        this.ChristmasGift = ChristmasGift;
+    }
+
+
+}
---
+++ GuiChristmasListapp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guichristmaslist;
+
+/**
+ *
+ * @author porte
+ */
+public class GuiChristmasListapp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       GuiChristmas l = new GuiChristmas();
+       l.setVisible(true);
+    }
+    
+}
